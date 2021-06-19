4e9d0cc466978f07e51fef34f66d18b29e9aca08
Lil' Sheffield <masheffield13@gmail.com>
2021-06-19 08:04:05 -0600

    commit 06.19

---
+++ animate.js
@@ -0,0 +1,8 @@
+console.log("Your js file is loaded correctly!")
+
+$(document).ready(function () {
+ 
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
